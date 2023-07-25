c5de0a0f133f7500b5219c1ad561ba9e488051fe
yuvarajsingh-0 <yuvarajsingh170@gmail.com>
2023-07-25 18:32:31 +0000

    building the board

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import "./styles.css";
-
-export default function App() {
+export default function Board() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
   );
 }
